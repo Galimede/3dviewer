c9a842dbf1f140bcfc3f50c8e63a3dfc64ee732c
Dubois Guillaume <guillaume.dubois@etudiant.univ-lille1.fr>
2018-10-17 14:18:53 +0200

    changement affichage

--- src/IHM/Display2D.java
+++ src/IHM/Display2D.java
@@ -2,15 +2,12 @@ package IHM;
 
 import java.awt.Toolkit;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import main.Face;
 import main.Model;
@@ -19,44 +16,44 @@ import tools.PlyReader;
 public class Display2D extends Application{
 	
 	private static Model modele;
-	private ArrayList<Line> lignes=new ArrayList<>();
 	
 
 
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		final int CENT4 =100*5;
-		try {
+	public void start(Stage primaryStage) {
+		int cpt=1;
+		double hScreen=Toolkit.getDefaultToolkit().getScreenSize().getHeight()-85;
+		double wScreen=Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		HBox root = new HBox();
+		Canvas canvas = new Canvas();
+		canvas.setHeight(hScreen);
+		canvas.setWidth(wScreen);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		root.getChildren().add(canvas);
+		double x=700;
+		double y= 450;
 		for (Face f : modele.getFaces()) {
-			double xp1 = f.getP1().getX();
-			double xp2 = f.getP2().getX();
-			double xp3 = f.getP3().getX();
-			double yp1 = f.getP1().getY();
-			double yp2 = f.getP2().getY();
-			double yp3 = f.getP3().getY();
-			lignes.add(new Line(xp1+CENT4,yp1+CENT4,xp2+CENT4,yp2+CENT4));
-			lignes.add(new Line(xp1+CENT4,yp1+CENT4,xp3+CENT4,yp3+CENT4));
-			lignes.add(new Line(xp3+CENT4,yp3+CENT4,xp2+CENT4,yp2+CENT4));
+			System.out.println("polygon"+ cpt+ "  "+(f.getP1().getX()+x));
+			cpt++;
+			gc.strokePolygon(	new double[] {f.getP1().getX()+x,f.getP2().getX()+x,f.getP3().getX()+x},
+								new double[] {f.getP1().getY()+y,f.getP2().getY()+y,f.getP3().getY()+y},
+								3);
 		}
-		}catch (Exception e) {
-			System.out.println("l'erreur est :" + e.getMessage());
-		}
-		Group root = new Group();
-		root.getChildren().addAll(lignes);
-		/*VBox v = new VBox();
-		Label tourner = new Label("Tourner");
-		Button b1 = new Button();
-		Label zoom = new Label("Zoom");
-		Button zoomPlus = new Button();
-		Button zoomMoins = new Button();
-		Label translater = new Label("Translater");
-		Button b2 = new Button();*/
+		
+		//VBox v = new VBox();
+		//Label tourner = new Label("Tourner");
+		//Button b1 = new Button();
+		//Label zoom = new Label("Zoom");
+		//Button zoomPlus = new Button();
+		//Button zoomMoins = new Button();
+		//Label translater = new Label("Translater");
+		//Button b2 = new Button();
 		Scene main = new Scene(root);
 		primaryStage.setScene(main);
 		primaryStage.setTitle("Dolphin");
-		primaryStage.setMinHeight(Toolkit.getDefaultToolkit().getScreenSize().getHeight()-85);
-		primaryStage.setWidth(Toolkit.getDefaultToolkit().getScreenSize().getWidth());
+		primaryStage.setMinHeight(hScreen);
+		primaryStage.setWidth(wScreen);
 		primaryStage.show();
 		
 	}
