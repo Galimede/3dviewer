6e0406e9f0655e6530fc251a6a93cebdf3ad4c6b
Dubois Guillaume <guillaume.dubois@etudiant.univ-lille1.fr>
2018-10-22 11:09:20 +0200

    début ihm, changement affichage

--- src/IHM/Display2D.java
+++ src/IHM/Display2D.java
@@ -10,7 +10,11 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import main.Face;
 import main.Model;
@@ -24,25 +28,28 @@ public class Display2D implements Observer{
 	public Display2D(Model modele) {
 		modele.addObserver(this);
 		Stage primaryStage = new Stage();
-		
 		double hScreen=Toolkit.getDefaultToolkit().getScreenSize().getHeight()-85;
 		double wScreen=Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		HBox root = new HBox();
+		root.setPrefSize(wScreen, hScreen);
 		Canvas canvas = new Canvas();
 		canvas.setHeight(hScreen);
-		canvas.setWidth(wScreen);
+		canvas.setWidth(wScreen-200);
 		gc = canvas.getGraphicsContext2D();
-		root.getChildren().add(canvas);
+		VBox v = new VBox();
+		v.setPrefSize(200, 200);
+		Button rotation = new Button("Rotation");
+		HBox h= new HBox();
+		Label zoom = new Label("Zoom");
+		Button zoomPlus = new Button("+");
+		Button zoomMoins = new Button("-");
+		Button translation = new Button("Translation");
+		h.getChildren().addAll(zoomMoins,zoomPlus);
+		v.getChildren().addAll(rotation,zoom,h,translation);
+		root.getChildren().addAll(v,canvas);
 		affichage(modele.getFaces());
 		
-		//VBox v = new VBox();
-		//Label tourner = new Label("Tourner");
-		//Button b1 = new Button();
-		//Label zoom = new Label("Zoom");
-		//Button zoomPlus = new Button();
-		//Button zoomMoins = new Button();
-		//Label translater = new Label("Translater");
-		//Button b2 = new Button();
+		
 		Scene main = new Scene(root);
 		primaryStage.setScene(main);
 		primaryStage.setTitle("Dolphin");
@@ -53,9 +60,11 @@ public class Display2D implements Observer{
 	}
 	
 	public void affichage (ArrayList<Face> faces){
+		gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+		gc.setStroke(Color.BLACK);
 		int cpt=1;
 		double x=700;
-		double y= 450;
+		double y= 400;
 		for (Face f : faces) {
 			System.out.println("polygon"+ cpt+ "  "+(f.getP1().getX()+x));
 			cpt++;
@@ -63,6 +72,7 @@ public class Display2D implements Observer{
 								new double[] {f.getP1().getY()+y,f.getP2().getY()+y,f.getP3().getY()+y},
 								3);
 		}
+		
 	}
 
 	@Override
