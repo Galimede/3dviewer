7007f72bdb078672425c9b8d0d60efaad5331fd6
dejonghg <dejonghg@bouleau14.iut-infobio.priv.univ-lille1.fr>
2018-10-22 11:09:40 +0200

    Passage de la classe point et de ses 3 coordoones via une matrice et ajout de la javadoc correspondante

--- src/main/Point.java
+++ src/main/Point.java
@@ -1,16 +1,15 @@
 package main;
 
 public class Point {
-	private double x;
-	private double y;
-	private double z;
+	private double[][]matrice= {{0.0,0.0,0.0,0.0},
+								{0.0,0.0,0.0,0.0},
+								{0.0,0.0,0.0,0.0},
+								{0.0,0.0,0.0,1.0}
+	};
 	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
-		return "Point [x=" + x + ", y=" + y + ", z=" + z + "]";
+		return "Point [x=" + matrice[0][0] + ", y=" + matrice[1][1] + ", z=" + matrice[2][2] + "]";
 	}
 	/**
 	 * @param x Coordonnee en largeur d'un point
@@ -18,26 +17,39 @@ public class Point {
 	 * @param z Coordonnee en profondeur d'un point
 	 */
 	public Point(double x, double y, double z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+		matrice[0][1]=x;
+		matrice[1][1]=y;
+		matrice[2][2]=z;
 	}
 	/**
 	 * @return Renvoie l'ordonnee du point 
 	 */
 	public double getX() {
-		return x;
+		return matrice[0][0];
 	}	
 	/**
 	 * @return Renvoie l'ordonnée du point
 	 */
 	public double getY() {
-		return y;
+		return matrice[1][1];
 	}
 	/**
 	 * @return Renvoie la coordonnée en profondeur du point
 	 */
 	public double getZ() {
-		return z;
+		return matrice[2][2];
+	}
+	/**
+	 * @return la matrice 3D du point correspondant
+	 */
+	public double[][]getMatrice(){
+		return matrice;
+	}
+	/**
+	 * Change les coordonnées du point via sa matrice
+	 * @param matrice la nouvelle matrice qui sera associe au point
+	 */
+	public void setMatrice(double[][]matrice){
+		this.matrice=matrice;
 	}
 }
