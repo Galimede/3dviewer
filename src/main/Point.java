db022d65a18e86c09a60f6b88784c170c5d3b693
grego <grego@LAPTOP-PBMU4OM5>
2019-01-03 23:41:48 +0100

    Update de la documentation de la classe Point

--- src/main/Point.java
+++ src/main/Point.java
@@ -9,6 +9,7 @@ public class Point {
 		return "Point [x=" + x + ", y=" + y + ", z=" + z+ "]";
 	}
 	/**
+	 * Construit un point a partir de trois coordonnes
 	 * @param x Coordonnee en largeur d'un point
 	 * @param y	Coordonnee en hauteur d'un point
 	 * @param z Coordonnee en profondeur d'un point
@@ -18,25 +19,32 @@ public class Point {
 		this.y=y;
 		this.z=z;
 	}
+	/**
+	 * Contruit un point a partir d'une matrice de taille 3x1 contenant les coordonnes de ce dernier
+	 * @param point Matrice 3x1 avec les coordonnees du point
+	 */
 	public Point (double[][]point) {
 		x=point[0][0];
 		y=point[1][0];
 		z=point[2][0];
 	}
 	/**
-	 * @return Renvoie l'ordonnée du point 
+	 * Renvoie la coordonnee en X du point
+	 * @return l'abcisse du point
 	 */
 	public double getX() {
 		return this.x;
 	}	
 	/**
-	 * @return Renvoie l'ordonnée du point
+	 * Renvoie la coordonnee en Y du point
+	 * @return l'ordonnee du point
 	 */
 	public double getY() {
 		return this.y;
 	}
 	/**
-	 * @return Renvoie la coordonnée en profondeur du point
+	 * Renvoie la coordonnee en Z du point
+	 * @return la profondeur du point
 	 */
 	public double getZ() {
 		return this.z;
